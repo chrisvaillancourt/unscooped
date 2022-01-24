689e872f01d5f1490a33a47ddf09a696675e23e6
Chris Vaillancourt <cvaillan22@gmail.com>
2022-01-23 22:55:01 -0800

    chore(app): fix formatting

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
-import { join } from 'path';
-import AutoLoad, {AutoloadPluginOptions} from 'fastify-autoload';
-import { FastifyPluginAsync } from 'fastify';
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+import AutoLoad, { AutoloadPluginOptions } from "fastify-autoload";
+import { FastifyPluginAsync } from "fastify";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export type AppOptions = {
   // Place your custom options for app below here.
 } & Partial<AutoloadPluginOptions>;
 
 const app: FastifyPluginAsync<AppOptions> = async (
-    fastify,
-    opts
+  fastify,
+  opts
 ): Promise<void> => {
   // Place here your custom code!
 
@@ -18,18 +22,17 @@ const app: FastifyPluginAsync<AppOptions> = async (
   // those should be support plugins that are reused
   // through your application
   void fastify.register(AutoLoad, {
-    dir: join(__dirname, 'plugins'),
-    options: opts
-  })
+    dir: join(__dirname, "plugins"),
+    options: opts,
+  });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   void fastify.register(AutoLoad, {
-    dir: join(__dirname, 'routes'),
-    options: opts
-  })
-
+    dir: join(__dirname, "routes"),
+    options: opts,
+  });
 };
 
 export default app;
-export { app }
+export { app };
